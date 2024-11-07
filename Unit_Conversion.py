87a2be2ec157b81991d60c86e3187898ff0ece41
tuotuoalan <33925778+tuotuoalan@users.noreply.github.com>
2024-11-08 01:28:17 +0800

    Update Unit_Conversion.py
    edit

--- Unit_Conversion.py
+++ Unit_Conversion.py
@@ -36,7 +36,7 @@ def NC_info(filepath):
     print(nf.variables)
     nf.close()
 
-#处理数据，将降水数据得单位m/hour转换为mm/day
+#处理数据，将降水数据得单位m转换为mm/day
 def ERA5_process_total_precipitation(file_path):  #这是原本采用的数据单位为m的时候的转换
     nc_trdata = Dataset(file_path, 'r+')
     # 数据*1000 ————>mm/day
